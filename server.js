a1b0d5da7feaef3f290c2cb499a1b4aff8051c36
Mitchell George <mitchgeorge8@gmail.com>
2022-05-03 20:03:18 -0400

    11.4.5 Test the functions

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require("fs");
-const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
@@ -11,8 +9,6 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
-
-const { animals } = require("./data/animals");
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
